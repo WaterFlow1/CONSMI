5cef2aab6567bae0b852558856c4d3004b118109
WaterFlow1 <154883275+WaterFlow1@users.noreply.github.com>
2023-12-27 18:40:28 +0800

    Add files via upload

--- utils/util.py
+++ utils/util.py
@@ -1,1 +1,12 @@
+# coding = utf-8
+import re
+import unicodedata
+from rdkit import Chem
 
+
+def augment(smile):
+    mols = set()
+    mol = Chem.MolFromSmiles(smile)
+    while len(mols) < 1:
+        mols.add(Chem.MolToSmiles(mol, doRandom=True))
+    return list(mols)
